646f0b3399ce4b7077b7e7f4c247d9904680a96b
Hung Le <hungle913@gmail.com>
2020-03-27 17:42:00 -0700

    added console logs to seperate results for easier reading

--- liri.js
+++ liri.js
@@ -62,6 +62,7 @@ function concertThis(input) {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
         for (var i = 0; i < response.data.length; i++) {
             // console.log(response.data);
+            console.log("-------------------------------------------------------");
             console.log("Name of the venue:", response.data[i].venue.name);
             console.log("Venue location:", response.data[i].venue.city);
             var eventDate = moment(response.data[i].datetime).format('MM/DD/YYYY');
@@ -82,6 +83,7 @@ function spotifySong(input) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        console.log("-------------------------------------------------------");
         console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
         console.log("Album: " + data.tracks.items[0].album.name);   
         console.log("Preview link of song: " + data.tracks.items[0].album.external_urls.spotify);  
